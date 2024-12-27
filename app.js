544c6b5dcbb3dd054411c9f5c7f25a47de3dcb3c
Theo <theo.lacanaria@gmail.com>
2024-12-27 22:18:45 +0800

    added campground.js for model, home.ejs for homepage templates and codes in app.js for connecting to database

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Campground = require('./models/campground')
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('home');
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard' });
+    await camp.save();
+    res.send(camp);
+
 })
 
 app.listen(3000, () => {
