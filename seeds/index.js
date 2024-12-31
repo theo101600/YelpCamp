d2ea41b462f1e347e1aaac73c546202f2c2099fb
Theo <theo.lacanaria@gmail.com>
2024-12-31 20:04:26 +0800

    styled edit.ejs, index.ejs, new.ejs and show.ejs using bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 100) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos dolorum cum magni ea possimus? Dolorem porro nesciunt dolorum quam unde ratione consequuntur neque, veniam, perspiciatis ut non ducimus in iure?',
+            price
         })
         await camp.save();
     }
