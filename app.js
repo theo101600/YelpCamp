849f025c0c324d5a55ff6de1e8ef6e61b76136d3
Theo <theo.lacanaria@gmail.com>
2025-02-03 12:09:10 +0800

    getting ready for deployment

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+//'mongodb://127.0.0.1:27017/yelp-camp'
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -32,6 +34,8 @@ db.once("open", () => {
 
 const path = require('path');
 const { title } = require('process');
+const MongoStore = require('connect-mongo');
+const { func } = require('joi');
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -42,10 +46,20 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
 
 const sessionConfig = {
+    store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
